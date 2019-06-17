14ff5c60993eef5161699118a10b7e919cf0426a
Jennifer V Phan <jennyphan181295@gmail.com>
2019-06-17 14:27:00 +0700

    first commit

--- app.js
+++ app.js
@@ -115,28 +115,28 @@ function moveBackward(rover) {
     switch (rover.direction) {
         case rover.direction = "N":
             if (rover.postY === 9) {
-                console.log("Rover is at border,cannot move forward in this direction!")
+                console.log("Rover is at border,cannot move backward in this direction!")
             } else {
                 rover.postY += 1;
             }
             break;
         case rover.direction = "S":
             if (rover.postY === 0) {
-                console.log("Rover is at border,cannot move forward in this direction!")
+                console.log("Rover is at border,cannot move backward in this direction!")
             } else {
                 rover.postY -= 1;
             }
             break;
         case rover.direction = "E":
             if (rover.postX === 0) {
-                console.log("Rover is at border,cannot move forward in this direction!")
+                console.log("Rover is at border,cannot move backward in this direction!")
             } else {
                 rover.postX -= 1;
             }
             break;
         case rover.direction = "W":
             if (rover.postX === 9) {
-                console.log("Rover is at border,cannot move forward in this direction!")
+                console.log("Rover is at border,cannot move backward in this direction!")
             } else {
                 rover.postX += 1;
             }
@@ -152,17 +152,17 @@ function obstacleAhead(rover) {
         var length = rover.travelLog.length;
         rover.postX = rover.travelLog[length - 1][0];
         rover.postY = rover.travelLog[length - 1][1];
-    } else if (roverA.postX == roverB.postX && roverA.postY == roverB.postY) {
+    } else if (roverA.postX === roverB.postX && roverA.postY === roverB.postY) {
         console.log("Cannot move, rover A & rover B will collide!");
         var length = rover.travelLog.length;
         rover.postX = rover.travelLog[length - 1][0];
         rover.postY = rover.travelLog[length - 1][1];
-    } else if (roverA.postX == roverC.postX && roverA.postY == roverC.postY) {
+    } else if (roverA.postX === roverC.postX && roverA.postY === roverC.postY) {
         console.log("Cannot move, rover A & rover C will collide!");
         var length = rover.travelLog.length;
         rover.postX = rover.travelLog[length - 1][0];
         rover.postY = rover.travelLog[length - 1][1];
-    } else if (roverB.postX == roverC.postX && roverB.postY == roverC.postY) {
+    } else if (roverB.postX === roverC.postX && roverB.postY === roverC.postY) {
         console.log("Cannot move, rover B & rover C will collide!");
         var length = rover.travelLog.length;
         rover.postX = rover.travelLog[length - 1][0];
